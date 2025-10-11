a6e08f5503206f9b77be7d04c64a7919f89536b7
Arman Dowlatabadi <Arman98DA@gmail.com>
2025-10-11 22:33:34 +0330

    Generate a random password of a specified length

---
+++ common/util/password.util.ts
@@ -0,0 +1,20 @@
+import { hashSync } from "bcrypt";
+
+export function randomPassword(length: number) {
+  let result = "";
+  const characters =
+    "@#$-+*!ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return {
+    password: result,
+    hashed: hashSync(result, 12),
+  };
+}
+export function hashPassword(data: string) {
+  return hashSync(data, 12);
+}
